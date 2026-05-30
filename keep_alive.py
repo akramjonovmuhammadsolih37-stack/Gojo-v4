b507f3f9ee2b2d614ea2e08a6909e6a390fc973a
zet <tdmuztaygertdmuztayger@gmail.com>
2026-05-30 18:09:02 +0500

    fix keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,3 @@
-"""
-Render free plan uchun keep-alive server
-Bot o'chib qolmasligi uchun HTTP server ham ishlatiladi
-"""
 from flask import Flask
 from threading import Thread
 
@@ -9,10 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "GOJO-USERBOT ishlayapti!"
+    return "Bot ishlayapti!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    import logging
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    app.run(host='0.0.0.0', port=8080, use_reloader=False)
 
 def keep_alive():
     t = Thread(target=run)
